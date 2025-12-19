3d84803317e6a925abcf1fed9b3b77037c358bc1
Lisandro778 <gutierrezlissandro677@gmail.com>
2025-12-19 13:01:51 -0600

    PROGRAMA POR LISSANDRO

---
+++ pesoeuro.py
@@ -0,0 +1,5 @@
+def pesos_a_euros():
+    print("ingresa monto a cambiar")
+    pesos = float(input("ingresa la cantidad de pesos que quieres cambiar: "))
+    cantidad=23.0
+    print(pesos / cantidad,"euros")
