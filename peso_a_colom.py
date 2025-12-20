aa0ce97bc17ec9edb0f4d09f8aadec6cba47943f
Lisandro778 <gutierrezlissandro677@gmail.com>
2025-12-19 19:13:05 -0600

    peso a coombiano

---
+++ peso_a_colom.py
@@ -0,0 +1,4 @@
+tasa_mx_a_cop = 0.0047
+pesos_mexicanos = float(input("ingresa la cantidad en pesos mx: "))
+pesos_colombianos = pesos_mexicanos * tasa_mx_a_cop
+print("equivale a ",pesos_colombianos,"pesos_colombianos")
